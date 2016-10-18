99ece9e233b3b63fea33964804af5e746ad0151b
Tsutomu Kawamura <cognitom@gmail.com>
2016-10-18 11:19:53 +0900

    Removes a log file after testing

--- test/main.spec.js
+++ test/main.spec.js
@@ -3,14 +3,18 @@ const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
 
+const logPath = path.join(process.cwd(), 'phantomjs.log')
+
 describe('webdriverio', function () {
   it('should run a test', function () {
     const title = browser.url('/').getTitle()
     assert.equal(title, 'WebdriverIO - Selenium 2.0 javascript bindings for nodejs')
   })
-  it('should have created a log file "phantomjs.log"', function(){
-    const filePath = path.join(process.cwd(), 'phantomjs.log')
-    const file = fs.statSync(filePath)
+  it('should have created a log file "phantomjs.log"', function () {
+    const file = fs.statSync(logPath)
     assert(file.size > 0)
+  })
+  after(function () {
+    fs.unlinkSync(logPath)
   })
 })
